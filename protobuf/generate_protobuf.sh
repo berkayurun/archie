6cccbc8e876384f1f8b89221baca2d5f0cdcb71b
Berkay Ürün <berkay.ueruen@aisec.fraunhofer.de>
2023-01-04 11:05:57 +0100

    Use protobuf for communication on control pipe
    Create protobuf schema for control pipe and update code to use protobuf
    messages for fault communication over control pipe.

--- protobuf/generate_protobuf.sh
+++ protobuf/generate_protobuf.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+protoc -I=./ --c_out=../faultplugin/ ./control.proto
+protoc -I=./ --python_out=../ --pyi_out=../ ./control.proto
+
 protoc -I=./ --c_out=../faultplugin/ ./fault.proto
 protoc -I=./ --python_out=../ --pyi_out=../ ./fault.proto
 
