28199ee2aafa322e833c0e7cf8e87a657fd42284
Berkay Ürün <berkay.ueruen@aisec.fraunhofer.de>
2023-01-11 01:05:07 +0100

    Use protobuf for communication on data pipe
    Create protobuf schema for communication on data pipe and update code to
    use protobuf for transfer the result information back to python side.
    
    Use enums for registers

--- protobuf/generate_protobuf.sh
+++ protobuf/generate_protobuf.sh
@@ -6,3 +6,6 @@ protoc -I=./ --python_out=../ --pyi_out=../ ./control.proto
 protoc -I=./ --c_out=../faultplugin/ ./fault.proto
 protoc -I=./ --python_out=../ --pyi_out=../ ./fault.proto
 
+protoc -I=./ --c_out=../faultplugin/ ./data.proto
+protoc -I=./ --python_out=../ --pyi_out=../ ./data.proto
+
