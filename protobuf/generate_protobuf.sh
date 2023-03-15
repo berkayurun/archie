49089bbc1be550d6205e6fac856d7febe1c8eccf
Berkay Ürün <berkay.ueruen@aisec.fraunhofer.de>
2023-03-15 14:21:33 +0100

    Move protobuf files into their own directory

--- protobuf/generate_protobuf.sh
+++ protobuf/generate_protobuf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-protoc -I=./ --c_out=../faultplugin/ ./control.proto
-protoc -I=./ --python_out=../ --pyi_out=../ ./control.proto
+protoc --c_out=../faultplugin/protobuf ./control.proto
+protoc --python_out=./ --pyi_out=./ ./control.proto
 
-protoc -I=./ --c_out=../faultplugin/ ./fault.proto
-protoc -I=./ --python_out=../ --pyi_out=../ ./fault.proto
+protoc --c_out=../faultplugin/protobuf ./fault.proto
+protoc --python_out=./ --pyi_out=./ ./fault.proto
 
-protoc -I=./ --c_out=../faultplugin/ ./data.proto
-protoc -I=./ --python_out=../ --pyi_out=../ ./data.proto
+protoc --c_out=../faultplugin/protobuf ./data.proto
+protoc --python_out=./ --pyi_out=./ ./data.proto
 
